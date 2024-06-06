4389b0b1fb67d5bd934d886e8b314db92c17d0d0
elyorbekanvarov <e0275king@gmail.com>
2024-06-06 12:55:21 +0500

    10-dars

---
+++ js/index.js
@@ -0,0 +1,94 @@
+// 1 - masala
+
+// let massiv = [1, 2, 3, 4, 5];
+// let yangiMassiv = [];
+// massiv.forEach(element => {
+//     yangiMassiv.push(element * 2);
+// });
+// console.log(yangiMassiv);
+
+// 2 - masala
+
+// function musbatQiymatlar(massiv) {
+//     let yangiMassiv = [];
+//     for (var i = 0; i < massiv.length; i++) {
+//         if (massiv[i] >= 0) {
+//             yangiMassiv.push(massiv[i]);
+//         }
+//     }
+//     return yangiMassiv;
+// }
+// let berilganMassiv = [-1, 2, -3, 4, -5, 6];
+// let yangiMassiv = musbatQiymatlar(berilganMassiv);
+// console.log(yangiMassiv);
+
+// 3 - masala
+
+// let numbers = [1, 2, 3, 4, 5];
+// numbers.forEach(function(element) {
+//   console.log(element * 2);
+// });
+
+// 4 - masala
+
+// function massivYigindisi(massiv) {
+//     let yigindi = 0;
+//     for (let i = 0; i < massiv.length; i++) {
+//         yigindi += massiv[i];
+//     }
+//     return yigindi;
+// }
+// let testMassiv = [1, 2, 3, 4, 5];
+// let natija = massivYigindisi(testMassiv);
+// console.log(natija);
+
+// 5 - masala
+
+// function filterPositive(arr) {
+//     return arr.filter(item => item > 0);
+// }
+// const originalArray = [-3, 5, -7, 10, -2, 8];
+// const positiveArray = filterPositive(originalArray);
+// console.log(originalArray);
+// console.log(positiveArray);
+
+// 6 - masala
+
+// const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const evenNumbers = inputArray.filter(num => num % 2 === 0);
+// console.log(evenNumbers);
+
+// 7 - masala
+
+// const berilganMassiv = ["apple", "banana", "pear", "grape", "kiwi", "orange", "melon"];
+// const yangiMassiv = berilganMassiv.filter(item => item.length < 5);
+// console.log(yangiMassiv);
+
+// 8 - masala
+
+// let odamlar = [
+//     {ism: 'Ali', yosh: 25},
+//     {ism: 'Vali', yosh: 17},
+//     {ism: 'Dilnoza', yosh: 20},
+//     {ism: 'Olim', yosh: 16}
+// ];
+// let kattaYoshdagiOdamlar = odamlar.filter(odam => odam.yosh > 18);
+// console.log(kattaYoshdagiOdamlar);
+
+// 9 - masala
+
+// let numbers = [1, 2, 3, 4, 5];
+// let squaredNumbers = numbers.map(num => num * num);
+// console.log(squaredNumbers);
+
+// 10 - masala
+
+// let inputArray = [1, 2, 3, 4, 5];
+// let outputArray = inputArray.map(item => item + 5);
+// console.log(outputArray);
+
+// 11 - masala
+
+// let numbers = [1, 2, 3, 4, 5];
+// let squaredNumbers = numbers.map((num) => num * num);
+// console.log(squaredNumbers);
